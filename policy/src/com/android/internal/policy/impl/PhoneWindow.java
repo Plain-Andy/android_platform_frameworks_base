72eb6f7e5d938ed8a70b044bfaacd4f235ff3b94
John Spurlock <jspurlock@google.com>
2013-10-18 17:34:42 -0400

    Move the IME navigation guard view up to decor.
    Although the IME windows are now allowed to extend into
    the nav bar, some IMEs were making assumptions about
    computed insets based on the height of the content view.
    
    So our navigation bar view (opaque view blocking the nav bar
    area to avoid the island effect when transparent) needs to live
    above the content view in the hierarchy, making the content view
    the same height as it was before.
    
    A surgical spot to put the guard view is up at the root view
    (PhoneWindow.DecorView).  fitSystemWindows is always called since
    this view is not recreated, and the layout is stable: waiting until
    the IME is attached to the window is too late to add a guard view.
    
    This is above the screen_* layouts, so will work without having to
    touch all of them.  And it only affects windows of TYPE_INPUT_METHOD.
    
    Bug:11237795
    Change-Id: I6a93f30aec83f1cecfb854073046cbc87ab4aa66

--- policy/src/com/android/internal/policy/impl/PhoneWindow.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindow.java
@@ -1922,9 +1922,7 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
         private PopupWindow mActionModePopup;
         private Runnable mShowActionModePopup;
 
-        // View added at runtime to draw under the status bar area
-        private View mStatusGuard;
-        // View added at runtime to draw under the navigation bar area
+        // View added at runtime to IME windows to cover the navigation bar
         private View mNavigationGuard;
 
         public DecorView(Context context, int featureId) {
@@ -2486,8 +2484,33 @@ public class PhoneWindow extends Window implements MenuBuilder.Callback {
         @Override
         protected boolean fitSystemWindows(Rect insets) {
             mFrameOffsets.set(insets);
-            updateStatusGuard(insets);
-            updateNavigationGuard(insets);
+
+            // IMEs lay out below the nav bar, but the content view must not (for back compat)
+            if (getAttributes().type == WindowManager.LayoutParams.TYPE_INPUT_METHOD) {
+                // prevent the content view from including the nav bar height
+                if (mContentParent != null) {
+                    if (mContentParent.getLayoutParams() instanceof MarginLayoutParams) {
+                        MarginLayoutParams mlp =
+                                (MarginLayoutParams) mContentParent.getLayoutParams();
+                        mlp.bottomMargin = insets.bottom;
+                        mContentParent.setLayoutParams(mlp);
+                    }
+                }
+                // position the navigation guard view, creating it if necessary
+                if (mNavigationGuard == null) {
+                    mNavigationGuard = new View(mContext);
+                    mNavigationGuard.setBackgroundColor(mContext.getResources()
+                            .getColor(R.color.input_method_navigation_guard));
+                    addView(mNavigationGuard, new LayoutParams(
+                            LayoutParams.MATCH_PARENT, insets.bottom,
+                            Gravity.START | Gravity.BOTTOM));
+                } else {
+                    LayoutParams lp = (LayoutParams) mNavigationGuard.getLayoutParams();
+                    lp.height = insets.bottom;
+                    mNavigationGuard.setLayoutParams(lp);
+                }
+            }
+
             if (getForeground() != null) {
                 drawableChanged();
             }
