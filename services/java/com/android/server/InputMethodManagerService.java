61a482e6591151d2440f17369598e823f47241d5
John Spurlock <jspurlock@google.com>
2013-10-23 16:36:47 -0400

    Decouple IME_ACTIVE and IME_VISIBLE for hardware keyboards.
    Reserve IME_VISIBLE for when software keyboards are actually
    showing.
    
    Bug:11186297
    Change-Id: I4e09a7de353e564bd1a97dadd603d3104254fa56

--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -876,9 +876,12 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
         final boolean hardKeyShown = haveHardKeyboard
                 && conf.hardKeyboardHidden
                         != Configuration.HARDKEYBOARDHIDDEN_YES;
-
-        final boolean isScreenLocked = isKeyguardLocked();
-        final boolean inputActive = !isScreenLocked && (mInputShown || hardKeyShown);
+        final boolean isScreenLocked =
+                mKeyguardManager != null && mKeyguardManager.isKeyguardLocked();
+        final boolean isScreenSecurelyLocked =
+                isScreenLocked && mKeyguardManager.isKeyguardSecure();
+        final boolean inputShown = mInputShown && (!isScreenLocked || mInputBoundToKeyguard);
+        final boolean inputActive = !isScreenSecurelyLocked && (inputShown || hardKeyShown);
         // We assume the softkeyboard is shown when the input is active as long as the
         // hard keyboard is not shown.
         final boolean inputVisible = inputActive && !hardKeyShown;
