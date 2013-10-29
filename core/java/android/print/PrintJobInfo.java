e674a684a4984d3430b121e1ba4c1c779146b335
Svetoslav Ganov <svetoslavganov@google.com>
2013-10-29 23:13:39 +0000

    Revert "Update the documentaton of the android.print package."
    This reverts commit 30c9eae16ef6b9d016fd8c8f8e60c331f587cac5.
    
    We do not need this as it appears that the SDK docs will be built from the klp-dev branch.
    
    Change-Id: Id0e288763eb2cb4a1e01283809fba0ea577c354c

--- core/java/android/print/PrintJobInfo.java
+++ core/java/android/print/PrintJobInfo.java
@@ -23,10 +23,7 @@ import android.os.Parcelable;
 import java.util.Arrays;
 
 /**
- * This class represents the description of a print job. The print job
- * state includes properties such as its id, print attributes used for
- * generating the content, and so on. Note that the print jobs state may
- * change over time and this class represents a snapshot of this state.
+ * This class represents the description of a print job.
  */
 public final class PrintJobInfo implements Parcelable {
 
@@ -97,7 +94,7 @@ public final class PrintJobInfo implements Parcelable {
     public static final int STATE_BLOCKED = 4;
 
     /**
-     * Print job state: The print job is successfully printed.
+     * Print job state: The print job was successfully printed.
      * This is a terminal state.
      * <p>
      * Next valid states: None
@@ -107,14 +104,15 @@ public final class PrintJobInfo implements Parcelable {
 
     /**
      * Print job state: The print job was printing but printing failed.
+     * This is a terminal state.
      * <p>
-     * Next valid states: {@link #STATE_CANCELED}, {@link #STATE_STARTED}
+     * Next valid states: None
      * </p>
      */
     public static final int STATE_FAILED = 6;
 
     /**
-     * Print job state: The print job is canceled.
+     * Print job state: The print job was canceled.
      * This is a terminal state.
      * <p>
      * Next valid states: None
@@ -301,14 +299,6 @@ public final class PrintJobInfo implements Parcelable {
      * Gets the current job state.
      *
      * @return The job state.
-     *
-     * @see #STATE_CREATED
-     * @see #STATE_QUEUED
-     * @see #STATE_STARTED
-     * @see #STATE_COMPLETED
-     * @see #STATE_BLOCKED
-     * @see #STATE_FAILED
-     * @see #STATE_CANCELED
      */
     public int getState() {
         return mState;
@@ -675,7 +665,7 @@ public final class PrintJobInfo implements Parcelable {
          * Constructor.
          *
          * @param prototype Prototype to use as a starting point.
-         * Can be <code>null</code>.
+         * Can be null.
          */
         public Builder(PrintJobInfo prototype) {
             mPrototype = (prototype != null)
