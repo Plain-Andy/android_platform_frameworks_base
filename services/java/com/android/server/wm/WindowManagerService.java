6191d8447173cff796711609d136b29434cb6977
Craig Mautner <cmautner@google.com>
2013-11-08 15:24:22 -0800

    Fix a couple of issues with mOpeningApps.
    In setAppVisibility add wtoken to mOpeningApps even if the requested
    visibility already matches requestedHidden. When Keyguard hides an app
    requestedHidden will mismatch and mOpeningApps will end up empty where
    it should have the app that will become visible.
    
    Add mAnimateWallpaperWithTarget = true to another situation where
    wallpaper animation needs it.
    
    Fixes bug 11570753.
    
    Change-Id: I70b93bbb580386eb912613f0ce11e582eff8c449

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -8530,6 +8530,7 @@ public class WindowManagerService extends IWindowManager.Stub
                 // We are transitioning from an activity with
                 // a wallpaper to one without.
                 transit = AppTransition.TRANSIT_WALLPAPER_CLOSE;
+                mAnimateWallpaperWithTarget = true;
                 if (DEBUG_APP_TRANSITIONS) Slog.v(TAG,
                         "New transit away from wallpaper: " + transit);
             } else if (mWallpaperTarget != null && mWallpaperTarget.isVisibleLw()) {
