ebcbd08eb9a05c278491fdc5bef25c3e7b9615da
crondog <patches@crondog.com>
2013-11-14 19:46:37 +1100

    [2/2] Framework/base: Configure headset hook
    Whether or not to allo headset hook to launch voice commands
    
    Useful with headset control programs
    
    PS3: Dont use getIntForUser directly. It breaks
    PS5: Whitespce
    PS6: Rebase
    
    Change-Id: Ifc24407b87a469a4baadad098895564f7f9aecb8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2870,6 +2870,11 @@ public final class Settings {
          */
         public static final String KEY_APP_SWITCH_LONG_PRESS_ACTION = "key_app_switch_long_press_action";
 
+        /** Weather to allow headsethook to launch voice commands
+         * @hide
+         */
+        public static final String HEADSETHOOK_LAUNCH_VOICE = "headsethook_launch_voice";
+
         /**
          * Whether or not to launch default music player when headset is connected
          * @hide
