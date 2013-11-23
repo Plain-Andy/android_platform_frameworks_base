211607540388dd94421b077f771a18de630d9b26
aznrice <aznrice2k4@gmail.com>
2013-11-22 21:20:46 -0600

    Revert "bring in torchCostant"
    This reverts commit 18703ed232e706b8f73aa9d5406972c5bbbbd385.
    
    Change-Id: I19ee4c8f94cb3e0fbcb191250d89e8125e269825

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -63,7 +63,6 @@ import android.view.WindowManagerGlobal;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.internal.util.rascarlo.TorchConstants;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.ActivityState;
 import com.android.systemui.statusbar.phone.QuickSettingsModel.BluetoothState;
@@ -793,7 +792,7 @@ class QuickSettings {
             torchTile.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Intent intent = new Intent(TorchConstants.ACTION_TOGGLE_STATE);
+                    Intent intent = new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT");
                     mContext.sendBroadcast(intent);
                 }
             });
@@ -802,7 +801,9 @@ class QuickSettings {
                     @Override
                     public boolean onLongClick(View v) {
                         getService().animateCollapsePanels();
-                        startSettingsActivity(TorchConstants.INTENT_LAUNCH_APP);
+                        Intent intent = new Intent(Intent.ACTION_MAIN);
+                        intent.setClassName("net.cactii.flash2", "net.cactii.flash2.MainActivity");
+                        startSettingsActivity(intent);
                         return true;
                     }
                 });
